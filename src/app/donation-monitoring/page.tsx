12140a8d6ddf8bdbe93a7c53fc4e4613c50a75d1
JhayStark <jaamen5@gmail.com>
2024-04-12 13:37:08 +0000

    fix: switched page routes

---
+++ src/app/donation-monitoring/page.tsx
@@ -0,0 +1,95 @@
+'use client';
+
+import { useState } from 'react';
+import { BadgeCent, HandCoins, UsersRound } from 'lucide-react';
+import Link from 'next/link';
+import DatatableBase from '@/app/DatatableBase';
+import api from '@/lib/axiosInstance';
+import useSWR from 'swr';
+import { formatCurrencyToGHS } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
+
+const fetcher = (url: string) => api.get(url).then(res => res.data);
+
+const StatsCard = ({
+  title,
+  value,
+  hexcode,
+  textColor,
+  icon,
+}: {
+  title: string;
+  value?: number | string;
+  hexcode: string;
+  textColor?: string;
+  icon?: any;
+}) => (
+  <div
+    className={` flex flex-col w-full ${hexcode} py-8 px-7 rounded-2xl gap-5`}
+  >
+    <div className='flex flex-row items-center justify-between'>
+      <h2
+        className={`${textColor} lg:text-[0.790rem] xl:text-[0.901rem] 2xl:text-[1.145rem]`}
+      >
+        {title}
+      </h2>
+      {icon}
+    </div>
+    <div className='flex flex-row items-center justify-between'>
+      <p
+        className={`${textColor} lg:text-[0.790rem] xl:text-[0.901rem] 2xl:text-[1.145rem]`}
+      >
+        {value}
+      </p>
+    </div>
+  </div>
+);
+
+export default function Home() {
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const { data, error, isLoading } = useSWR(
+    `/donations?page=${pageNumber}`,
+    fetcher
+  );
+  const { data: statsData } = useSWR(`/donations/stats`, fetcher);
+
+  return (
+    <>
+      <div className='w-full  gap-3  flex flex-col md:flex-row  items-end mb-5 lg:mb-20'>
+        <Link href='/'>
+          <button className='flex items-center font-semibold cursor-pointer hover:opacity-60 text-lg w-52 mb-2 gap-3 '>
+            <HandCoins />
+            <span>Collect Donation</span>
+          </button>
+        </Link>
+        <StatsCard
+          title='Total Donations'
+          hexcode='bg-[#043F2E]'
+          textColor='text-[#FFFFE2]'
+          value={formatCurrencyToGHS(statsData?.total) || 'GHS 0'}
+          icon={<BadgeCent className='text-white' />}
+        />
+        <StatsCard
+          title='Total Donors'
+          hexcode='border-[#FFD249] border-2 bg-inherit'
+          textColor='text-white'
+          value={
+            statsData?.count
+              ? new Intl.NumberFormat().format(statsData?.count)
+              : 0
+          }
+          icon={<UsersRound />}
+        />
+      </div>
+      <Link href={'/message'}>
+        <Button className='bg-blue-950 my-5'>Send thank you message</Button>
+      </Link>
+      <DatatableBase
+        data={data}
+        setPageNumber={setPageNumber}
+        pageNumber={pageNumber}
+      />
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,167 @@
 'use client';
 
-import { useState } from 'react';
-import { BadgeCent, HandCoins, UsersRound } from 'lucide-react';
-import Link from 'next/link';
-import DatatableBase from './DatatableBase';
-import api from '@/lib/axiosInstance';
-import useSWR from 'swr';
-import { formatCurrencyToGHS } from '@/lib/utils';
+import React, { useState } from 'react';
+import { Form } from '@/components/ui/form';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { InputFormField, SelectFormField } from '@/components/formFields';
 import { Button } from '@/components/ui/button';
+import { CircleArrowLeft } from 'lucide-react';
+import { useToast } from '@/components/ui/use-toast';
+import Link from 'next/link';
+import api from '@/lib/axiosInstance';
+const defaultValues = {
+  name: '',
+  contact: '',
+  amount: '',
+  recipientName: '',
+  recipientType: '',
+};
+const formSchema = z.object({
+  name: z.string().min(1, { message: 'Donor name required' }),
+  contact: z.string().min(1, { message: 'Donor contact required' }),
+  amount: z
+    .string()
+    .min(1, { message: 'Amount must be 1 or more' })
+    .refine(
+      value => {
+        // Convert value to a number for comparison
+        const numericValue = parseFloat(value);
+        // Check if the numeric value is less than 1
+        return numericValue >= 1;
+      },
+      {
+        message: 'Amount must be 1 or more',
+      }
+    ),
+  recipientName: z.string(),
+  recipientType: z.string().min(1, { message: 'Recipient type required' }),
+});
 
-const fetcher = (url: string) => api.get(url).then(res => res.data);
+const Page = () => {
+  const [loading, setLoading] = useState(false);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: '',
+      contact: '',
+      amount: '',
+      recipientName: '',
+      recipientType: '',
+    },
+  });
+  const { toast } = useToast();
+  const submitForm = async (data: z.infer<typeof formSchema>) => {
+    setLoading(true);
+    try {
+      await api.post('/donations/add', data);
+      toast({
+        title: 'Donation Recieved',
+        description: `Donation of GHS ${data.amount} recieved from ${data.name}`,
+      });
+      setLoading(false);
+      form.reset(defaultValues);
+    } catch (error) {
+      toast({
+        title: 'Donation Failed',
+        description: `Donation of GHS ${data.amount} failed from ${data.name}`,
+        variant: 'destructive',
+      });
+      setLoading(false);
+    }
+  };
 
-const StatsCard = ({
-  title,
-  value,
-  hexcode,
-  textColor,
-  icon,
-}: {
-  title: string;
-  value?: number | string;
-  hexcode: string;
-  textColor?: string;
-  icon?: any;
-}) => (
-  <div
-    className={` flex flex-col w-full ${hexcode} py-8 px-7 rounded-2xl gap-5`}
-  >
-    <div className='flex flex-row items-center justify-between'>
-      <h2
-        className={`${textColor} lg:text-[0.790rem] xl:text-[0.901rem] 2xl:text-[1.145rem]`}
-      >
-        {title}
-      </h2>
-      {icon}
-    </div>
-    <div className='flex flex-row items-center justify-between'>
-      <p
-        className={`${textColor} lg:text-[0.790rem] xl:text-[0.901rem] 2xl:text-[1.145rem]`}
-      >
-        {value}
-      </p>
-    </div>
-  </div>
-);
-
-export default function Home() {
-  const [pageNumber, setPageNumber] = useState(1);
-
-  const { data, error, isLoading } = useSWR(
-    `/donations?page=${pageNumber}`,
-    fetcher
+  const renderInputField = (
+    label: string,
+    fieldName: string,
+    placeholder?: string,
+    type?: string
+  ) => (
+    <InputFormField
+      label={label}
+      form={form}
+      name={fieldName}
+      type={type}
+      placeholder={placeholder}
+    />
   );
-  const { data: statsData } = useSWR(`/donations/stats`, fetcher);
 
+  const renderSelectField = (
+    label: string,
+    fieldName: string,
+    options?: string[],
+    placeholder?: string
+  ) => (
+    <SelectFormField
+      label={label}
+      form={form}
+      name={fieldName}
+      options={options}
+      placeholder={placeholder}
+    />
+  );
   return (
-    <>
-      <div className='w-full  gap-3  flex flex-col md:flex-row  items-end mb-5 lg:mb-20'>
-        <Link href='/add'>
-          <button className='flex items-center font-semibold cursor-pointer hover:opacity-60 text-lg w-52 mb-2 gap-3 '>
-            <HandCoins />
-            <span>Collect Donation</span>
+    <Form {...form}>
+      <div className='pb-5'>
+        {/* <Link href='/'>
+          <button className='flex items-center gap-5'>
+            <CircleArrowLeft />
+            Back
           </button>
-        </Link>
-        <StatsCard
-          title='Total Donations'
-          hexcode='bg-[#043F2E]'
-          textColor='text-[#FFFFE2]'
-          value={formatCurrencyToGHS(statsData?.total) || 'GHS 0'}
-          icon={<BadgeCent className='text-white' />}
-        />
-        <StatsCard
-          title='Total Donors'
-          hexcode='border-[#FFD249] border-2 bg-inherit'
-          textColor='text-white'
-          value={
-            statsData?.count
-              ? new Intl.NumberFormat().format(statsData?.count)
-              : 0
-          }
-          icon={<UsersRound />}
-        />
+        </Link> */}
+        <h1 className='text-lg lg:text-3xl font-semibold text-center'>
+          Donation Form
+        </h1>
       </div>
-      <Link href={'/message'}>
-        <Button className='bg-blue-950 my-5'>Send thank you message</Button>
-      </Link>
-      <DatatableBase
-        data={data}
-        setPageNumber={setPageNumber}
-        pageNumber={pageNumber}
-      />
-    </>
+      <form
+        className='w-full mx-auto max-w-3xl h-[80%]  md:h-[85%] flex-grow flex flex-col justify-between'
+        onSubmit={form.handleSubmit(submitForm)}
+        autoComplete='off'
+      >
+        <div className='space-y-10'>
+          {renderInputField('Donor Name', 'name', 'Enter name of donor')}
+          {renderInputField('Donor Contact', 'contact', '0240000017')}
+          {renderInputField(
+            'Amount',
+            'amount',
+            'Enter donation amount in cedis',
+            'number'
+          )}
+          {renderSelectField(
+            'Recipient Type',
+            'recipientType',
+            ['Family', 'Spouse', 'Children', 'Individual (Enter name below)'],
+            'Select recipient type'
+          )}
+          {form.watch('recipientType').includes('Individual') &&
+            renderInputField(
+              'Recipient Name',
+              'recipientName',
+              'Enter name of recipient'
+            )}
+        </div>
+
+        <Button
+          className='bg-white text-black hover:text-white w-full h-16'
+          type='submit'
+        >
+          {!loading ? (
+            'Submit'
+          ) : (
+            <div
+              className='inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]'
+              role='status'
+            >
+              <span className='!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]'>
+                Loading...
+              </span>
+            </div>
+          )}
+        </Button>
+      </form>
+    </Form>
   );
-}
+};
+
+export default Page;
